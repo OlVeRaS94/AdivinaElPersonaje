34e0b0a6308caf2cf2425cdee69ff66cd44ac8fd
OlVeRaS94 <juniortoni16@gmail.com>
2022-09-30 08:04:31 +0200

    Ultimos cambios y ocultar cosas

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-const CARDS = 10;
+const CARDS = 10; //constante para saber cuantos pokemon quiero adivinar
 
 // peticion de pokemon al API
 
 for (let i = 1; i <= CARDS; i++) {
-    let id = getRandomID(802)
-    searchPokemonById(id)//Busca las caracteristicas del pokémon
+    let id = getRandomID(802)//numero de pokemon
+    searchPokemonById(id)
 }
 
 function getRandomID(max) {
@@ -14,28 +14,31 @@ function getRandomID(max) {
 let draggableElements = document.querySelector('.draggable-elements')
 let droppableElements = document.querySelector('.droppable-elements')
 
-let pokemonSearched = [];
+let pokemonSearched = [];//busqueda del pokémon
 let pokemonNames = []
 async function searchPokemonById(id) {//funcion que me trae de una api los pokémon
-    //Las API son mecanismos que permiten a dos componentes de software comunicarse entre sí mediante un conjunto de definiciones y protocolos.
-    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
-    const data = await res.json()
+    //Las API son mecanismos que permiten a dos componentes de software comunicarse entre sí 
+    //mediante un conjunto de definiciones y protocolos.
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)//busqueda en la api los pokemon
+    const data = await res.json()//esperando la respuesta y almacenandola en la data
 
     //arreglos de los pokemon 
     pokemonSearched.push(data)
 
     //arreglo con los nombres de los pokemon
-    pokemonNames.push(data.name)
-    pokemonNames = pokemonNames.sort(() => Math.random() - 0.5)
+    pokemonNames.push(data.name)//busqueda de los nombres
+    pokemonNames = pokemonNames.sort(() => Math.random() - 0.5)//para tener los nombres desordenados
 
     // dibujando los pokemon
-    draggableElements.innerHTML = ''
+    draggableElements.innerHTML = ''//Borra y dibuja los elementos que tengamos en cards
     pokemonSearched.forEach(pokemon => {
         draggableElements.innerHTML +=
             `<div class="pokemon">
             <img id="${pokemon.name}" draggable="true" class="image" 
             src="${pokemon.sprites.other['official-artwork'].front_default}" alt="pokemon">
-            </div>`
+            </div>`//Pone las imagenes de forma dinamica de los pokemon
+
+
         //para hacer que sean shynis
         // draggableElements.innerHTML = ''
         //pokemonSearched.forEach(pokemon => {
@@ -60,7 +63,7 @@ async function searchPokemonById(id) {//funcion que me trae de una api los poké
     pokemons.forEach(pokemon => {
         pokemon.addEventListener('dragstart', event => {
             event.dataTransfer.setData('text', event.target.id)
-        })
+        })//para saber que estoy arrastrando la imagen y que al soltar coincida con el nombre
     })
 
     let names = document.querySelectorAll('.names')
@@ -70,11 +73,11 @@ async function searchPokemonById(id) {//funcion que me trae de una api los poké
     names.forEach(name => {
         name.addEventListener('dragover', event => {
             event.preventDefault()
-        })
+        })//para saber que he arrastrado la imagen y que al soltar coincida con el nombre
         name.addEventListener('drop', event => {
             const draggableElementData = event.dataTransfer.getData('text');
-            let pokemonElement = document.querySelector(`#${draggableElementData}`)
-            console.log(pokemonElement)
+            let pokemonElement = document.querySelector(`#${draggableElementData}`)// para saber que he soltado la imagen en el lugar correcto
+            //console.log(pokemonElement)
             if (event.target.innerText == draggableElementData) {
                 //console.log('SI')
                 points++
