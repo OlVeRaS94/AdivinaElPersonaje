5baff329d8a0a3d20e9b84e9c44a24487e6ece5b
Toni Oliveira Garcia <113018165+OlVeRaS94@users.noreply.github.com>
2022-10-01 20:48:41 +0200

    a ver si funcionan los pokemon normalmente

--- js/main.js
+++ js/main.js
@@ -31,26 +31,26 @@ async function searchPokemonById(id) {//funcion que me trae de una api los poké
 
     // dibujando los pokemon
     draggableElements.innerHTML = ''//Borra y dibuja los elementos que tengamos en cards
-     pokemonSearched.forEach(pokemon => {
-    draggableElements.innerHTML +=
-     `<div class="pokemon">
-     <img id="${pokemon.name}" draggable="true" class="image" 
-     src="${pokemon.sprites.other['official-artwork'].front_default}" alt="pokemon">
-     </div>`//Pone las imagenes de forma dinamica de los pokemon
-
-
-    //para hacer que sean shynis
-
-   // draggableElements.innerHTML = ''
-
-    //pokemonSearched.forEach(pokemon => {
-       // console.log(pokemon.sprites)
-       // draggableElements.innerHTML +=
-        //    `<div class="pokemon">
-            // <img id="${pokemon.name}" draggable="true" class="image" 
-           // src="${pokemon.sprites.other['home'].front_shiny}" alt="pokemon">
-         // </div>`
+    pokemonSearched.forEach(pokemon => {
+        draggableElements.innerHTML +=
+            `<div class="pokemon">
+            <img id="${pokemon.name}" draggable="true" class="image" 
+            src="${pokemon.sprites.other['official-artwork'].front_default}" alt="pokemon">
+            </div>`//Pone las imagenes de forma dinamica de los pokemon
     })
+
+        //para hacer que sean shynis
+
+        // draggableElements.innerHTML = ''
+
+        //pokemonSearched.forEach(pokemon => {
+        // console.log(pokemon.sprites)
+        // draggableElements.innerHTML +=
+        // `<div class="pokemon">
+        // <img id="${pokemon.name}" draggable="true" class="image" 
+        // src="${pokemon.sprites.other['home'].front_shiny}" alt="pokemon">
+        // </div>`
+    //})
 
     //poniendo los nombres a los pokemon
     droppableElements.innerHTML = ''
