ea2ea8a9315f5d144e99309443793fecf0aec371
Toni Oliveira Garcia <113018165+OlVeRaS94@users.noreply.github.com>
2022-10-01 20:37:17 +0200

    prueba?

--- js/main.js
+++ js/main.js
@@ -40,15 +40,15 @@ async function searchPokemonById(id) {//funcion que me trae de una api los poké
 
 
     //para hacer que sean shynis
-    
+
     draggableElements.innerHTML = ''
-    
+
     pokemonSearched.forEach(pokemon => {
         console.log(pokemon.sprites)
         draggableElements.innerHTML +=
             `<div class="pokemon">
          <img id="${pokemon.name}" draggable="true" class="image" 
-         src="${pokemon.sprites.front_shinys}" alt="pokemon">
+         src="${pokemon.sprites.other['home'].front_shiny}" alt="pokemon">
           </div>`
     })
 
