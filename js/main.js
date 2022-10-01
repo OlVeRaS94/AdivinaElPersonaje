9dc3e0f3aa7fc7b6e281785633746c4eea3d277b
Toni Oliveira Garcia <113018165+OlVeRaS94@users.noreply.github.com>
2022-10-01 20:03:02 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -30,23 +30,23 @@ async function searchPokemonById(id) {//funcion que me trae de una api los poké
     pokemonNames = pokemonNames.sort(() => Math.random() - 0.5)//para tener los nombres desordenados
 
     // dibujando los pokemon
-    draggableElements.innerHTML = ''//Borra y dibuja los elementos que tengamos en cards
-    pokemonSearched.forEach(pokemon => {
-        draggableElements.innerHTML +=
-            `<div class="pokemon">
-            <img id="${pokemon.name}" draggable="true" class="image" 
-            src="${pokemon.sprites.other['official-artwork'].front_default}" alt="pokemon">
-            </div>`//Pone las imagenes de forma dinamica de los pokemon
+   // draggableElements.innerHTML = ''//Borra y dibuja los elementos que tengamos en cards
+  // pokemonSearched.forEach(pokemon => {
+       // draggableElements.innerHTML +=
+           // `<div class="pokemon">
+          //  <img id="${pokemon.name}" draggable="true" class="image" 
+           // src="${pokemon.sprites.other['official-artwork'].front_default}" alt="pokemon">
+           // </div>`//Pone las imagenes de forma dinamica de los pokemon
 
 
         //para hacer que sean shynis
-        // draggableElements.innerHTML = ''
-        //pokemonSearched.forEach(pokemon => {
-        //draggableElements.innerHTML +=
-        //  `<div class="pokemon">
-        // <img id="${pokemon.name}" draggable="true" class="image" 
-        // src="${pokemon.sprites.front_shynis}" alt="pokemon">
-        //  </div>`
+         draggableElements.innerHTML = ''
+        pokemonSearched.forEach(pokemon => {
+        draggableElements.innerHTML +=
+          `<div class="pokemon">
+         <img id="${pokemon.name}" draggable="true" class="image" 
+         src="${pokemon.sprites.front_shynis}" alt="pokemon">
+          </div>`
     })
 
     //poniendo los nombres a los pokemon
